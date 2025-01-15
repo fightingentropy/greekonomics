07c91e8ada616c2a69243de3d0f1db211607dd81
Merlin <erlin.hx@gmail.com>
2025-01-15 14:08:29 +0000

    .

--- src/data/tweets.ts
+++ src/data/tweets.ts
@@ -13,7 +13,27 @@ export const tweets: Tweet[] = [
     author: 'chameleon_jeff'
   },
   {
+    url: 'https://x.com/trading_axe/status/1855128753053045122',
+    author: 'trading_axe'
+  },
+  {
+    url: 'https://x.com/0xinvicta/status/1879529540223213857',
+    author: '0xinvicta'
+  },
+  {
+    url: 'https://x.com/ercwl/status/1762873948847440333',
+    author: 'ercwl'
+  },
+  {
+    url: 'https://x.com/tier10k/status/1745184607421477312',
+    author: 'tier10k'
+  },
+  {
     url: 'https://x.com/CryptoCred/status/1879201483905749238',
     author: 'CryptoCred'
+  },
+  {
+    url: 'https://x.com/mikeykremer/status/1878261608738402464',
+    author: 'mikeykremer'
   }
 ];
