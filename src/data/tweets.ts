30203c49836dfbcf949c6079514177dd43f7ae1d
Merlin <erlin.hx@gmail.com>
2025-01-15 13:28:46 +0000

    .

--- src/data/tweets.ts
+++ src/data/tweets.ts
@@ -11,5 +11,9 @@ export const tweets: Tweet[] = [
   {
     url: 'https://x.com/chameleon_jeff/status/1879086717900386782',
     author: 'chameleon_jeff'
+  },
+  {
+    url: 'https://x.com/CryptoCred/status/1879201483905749238',
+    author: 'CryptoCred'
   }
 ];
